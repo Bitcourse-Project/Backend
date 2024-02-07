3e770c90e01f5d4c7a0970f7337764e2b87e21b3
alsensto <jmg1301@gmail.com>
2024-02-07 16:24:14 +0900

    first commit

---
+++ src/main/java/com/finance/bitcourse/BitcourseApplication.java
@@ -0,0 +1,13 @@
+package com.finance.bitcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitcourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitcourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/finance/bitcourse/BitcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.bitcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitcourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
